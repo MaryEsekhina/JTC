fe4884d78327ce59bffdd204537cb9cf166e9478
Mary <bukva_zu@list.ru>
2021-09-01 21:47:02 +0300

    JTC.ooo v.1

--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -3,8 +3,7 @@ package tests;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Configuration.baseUrl;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
@@ -39,7 +38,8 @@ public class Tests extends TestBase {
     void  aboutTest(){
         step("Открыть стартовую страницу", () -> open(baseUrl));
         step("Кликнуть \"О компании\"", () ->
-               /* $(".t228__list_item").$(".t-menu__link-item").*/$(withText("О компании")).shouldBe(visible,Duration.ofSeconds(3))).click();
+                $(withText("О компании"))
+                .shouldBe(visible,Duration.ofSeconds(3))).click();
         step("Проверить, что открылась страница \"О нас\"", () ->
                 assertThat($("[field=tn_text_1566803783349]").shouldHave(text("О нас"))));
     }
@@ -49,6 +49,17 @@ public class Tests extends TestBase {
         step("Выбрать пункт меню \"Продукты\"", () ->$(".t228__list_item .t-menusub__target-link").hover());
         step("Выбрать продукт \"Salto.Avanti\"", () ->$(byText("Salto.Avanti")).click());
         step("Проверить, что открылась страница \"ДБО для бизнеса\"", () ->$("[field=tn_text_1566803783349]").shouldHave(text("ДБО для бизнеса")));
+    }
+
+    @Test
+    void locationTest() {
+        step("Открыть стартовую страницу", () -> open(baseUrl));
+        step("Кликнуть по городу \"Рязань\"", () -> {
+            $(".t397__col").scrollTo();
+            $("[field=title4]").click();
+        });
+        step("Проверить, что адрес на ул. Маяковского", () -> $(byXpath("//div[@field='title' and contains(text(),'Рязань')]/..//div[@data-customstyle='yes']"))
+                .scrollTo().shouldHave(text("ул. Маяковского, дом 1А, литер А")));
     }
 
 
